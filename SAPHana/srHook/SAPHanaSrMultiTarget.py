36408822707c516bc2ae858d87f8adeabbad61b3
AngelaBriel <abriel@suse.com>
2024-01-19 14:00:56 +0100

    HA/DR provider hooks - make function logTimestamp() part of the class

--- SAPHana/srHook/SAPHanaSrMultiTarget.py
+++ SAPHana/srHook/SAPHanaSrMultiTarget.py
@@ -39,31 +39,32 @@ def getEpisode():
     episode = "{0}-{1}".format( datetime.now().strftime('%s') , random.randrange(10000,20000))
     return episode
 
-def logTimestamp(self, method, episode, outputMessage):
-    traceFilepath = os.path.join(os.environ['SAP_RETRIEVAL_PATH'], 'trace', 'nameserver_saphanasr_multitarget_hook.trc')
-    try:
-        with open(traceFilepath, "a", encoding='utf-8') as saphanasr_multitarget_file:
-            currentTimeStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f ')
-            outputMessage = "{0} [{2}] {1}".format(currentTimeStr,outputMessage, episode)
-            saphanasr_multitarget_file.write(outputMessage + "\n")
-            saphanasr_multitarget_file.flush()
-
-    except ( RuntimeError, TypeError, NameError, OSError ) as e :
-        self.tracer.info("{0}.{1}() logTimestamp error {2}".format(self.__class__.__name__, method, e))
-        print("Error in logTimestamp(): {0}".format(e))
-
 try:
     class SAPHanaSrMultiTarget(HADRBase):
         """
         SAPHanaSrMultiTarget implements the handling of the srConnectionChanged event
         """
 
+        def logTimestamp(self, method, episode, outputMessage):
+            traceFilepath = os.path.join(os.environ['SAP_RETRIEVAL_PATH'], 'trace', 'nameserver_saphanasr_multitarget_hook.trc')
+            try:
+                with open(traceFilepath, "a", encoding='utf-8') as saphanasr_multitarget_file:
+                    currentTimeStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f ')
+                    outputMessage = "{0} [{2}] {1}".format(currentTimeStr,outputMessage, episode)
+                    saphanasr_multitarget_file.write(outputMessage + "\n")
+                    saphanasr_multitarget_file.flush()
+
+            except ( RuntimeError, TypeError, NameError, OSError ) as e :
+                self.tracer.info("{0}.{1}() logTimestamp error {2}".format(self.__class__.__name__, method, e))
+                print("Error in logTimestamp(): {0}".format(e))
+
+
         def __init__(self, *args, **kwargs):
             # delegate construction to base class
             super(SAPHanaSrMultiTarget, self).__init__(*args, **kwargs)
             method = "init"
             episode = getEpisode()
-            logTimestamp(self, method, episode, "init called")
+            self.logTimestamp(self, method, episode, "init called")
             if self.config.hasKey("cib_access"):
                 self.cib_access = self.config.get("cib_access")
                 # first step, should be removed later
@@ -75,9 +76,9 @@ try:
             mySID = os.environ.get('SAPSYSTEMNAME')
             mysid = mySID.lower()
             myCMD = "sudo /usr/sbin/crm_attribute -n hana_{1}_gsh -v {0}  -l reboot".format(srHookGen, mysid)
-            logTimestamp(self, method, episode, "pre call " + myCMD)
+            self.logTimestamp(self, method, episode, "pre call " + myCMD)
             rc = os.system(myCMD)
-            logTimestamp(self, method, episode, "post call " + myCMD)
+            self.logTimestamp(self, method, episode, "post call " + myCMD)
             myMSG = "CALLING CRM: <{0}> rc={1}".format(myCMD, rc)
             self.tracer.info("{0}.{1}() {2}\n".format(self.__class__.__name__, method, myMSG))
             self.tracer.info("{0}.{1}() Running srHookGeneration {2}, see attribute hana_{3}_gsh too\n".format(self.__class__.__name__, method, srHookGen, mysid))
@@ -85,18 +86,18 @@ try:
             # check if multi-target support attribute exists
             mts = "true"
             myCMD = "sudo /usr/sbin/crm_attribute -n hana_%s_glob_mts -G" % (mysid)
-            logTimestamp(self, method, episode, "pre call " + myCMD)
+            self.logTimestamp(self, method, episode, "pre call " + myCMD)
             rc = os.system(myCMD)
-            logTimestamp(self, method, episode, "post call " + myCMD)
+            self.logTimestamp(self, method, episode, "post call " + myCMD)
             if rc != 0:
                 # multi-target support attribute not found, create it
                 myCMD = "sudo /usr/sbin/crm_attribute -n hana_{0}_glob_mts -v {1} -t crm_config -s SAPHanaSR".format(mysid, mts)
-                logTimestamp(self, method, episode, "pre call " + myCMD)
+                self.logTimestamp(self, method, episode, "pre call " + myCMD)
                 rc = os.system(myCMD)
-                logTimestamp(self, method, episode, "post call " + myCMD)
+                self.logTimestamp(self, method, episode, "post call " + myCMD)
                 myMSG = "CALLING CRM: <{0}> rc={1}".format(myCMD, rc)
                 self.tracer.info("{0}.{1}() {2}\n".format(self.__class__.__name__, method, myMSG))
-            logTimestamp(self, method, episode, "init exit")
+            self.logTimestamp(self, method, episode, "init exit")
 
 
         def about(self):
@@ -171,10 +172,10 @@ try:
             method = "srConnectionChanged"
             startTime = datetime.now()
             episode = getEpisode()
-            logTimestamp(self, method, episode, "srConnectionChanged called")
+            self.logTimestamp(self, method, episode, "srConnectionChanged called")
             """ finally we got the srConnection hook :) """
             self.tracer.info("{0}.{1}() method called with Dict={2} (version {3}) and cib_access {4}".format(self.__class__.__name__, method, ParamDict, fhSRHookVersion, self.cib_access))
-            logTimestamp(self, method, episode, "send dict message to log")
+            self.logTimestamp(self, method, episode, "send dict message to log")
             # myHostname = socket.gethostname()
             # myDatebase = ParamDict["database"]
             mySystemStatus = ParamDict["system_status"]
@@ -186,14 +187,14 @@ try:
             # if self.cib_access != "all-off" and self.cib_access != "glob-off":
             if self.cib_access == "all-on" or self.cib_access == "glob-on" or self.cib_access == "site-off":
                 myCMD = "sudo /usr/sbin/crm_attribute -n hana_{1}_gsh -v {0} -l reboot".format(srHookGen, mysid)
-                logTimestamp(self, method, episode, "pre call " + myCMD)
+                self.logTimestamp(self, method, episode, "pre call " + myCMD)
                 rc = os.system(myCMD)
-                logTimestamp(self, method, episode, "post call " + myCMD)
+                self.logTimestamp(self, method, episode, "post call " + myCMD)
                 myMSG = "CALLING CRM: <{0}> rc={1}".format(myCMD, rc)
                 self.tracer.info("{0}.{1}() {2}\n".format(self.__class__.__name__, method, myMSG))
-                logTimestamp(self, method, episode, "send result to log")
+                self.logTimestamp(self, method, episode, "send result to log")
                 self.tracer.info("{0}.{1}() Running srHookGeneration {2}, see attribute hana_{3}_gsh too\n".format(self.__class__.__name__, method, srHookGen, mysid))
-                logTimestamp(self, method, episode, "differ cases following dictionary entries")
+                self.logTimestamp(self, method, episode, "differ cases following dictionary entries")
             if mySystemStatus == 15:
                 mySRS = "SOK"
             else:
@@ -219,23 +220,23 @@ try:
                         # found global Hook attribute, write both (old and new) attributes
                         # for compatibility reasons
                         myCMD = "sudo /usr/sbin/crm_attribute -n hana_{0}_glob_srHook -v {1} -t crm_config -s SAPHanaSR".format(mysid, mySRS)
-                        logTimestamp(self, method, episode, "pre call " + myCMD)
+                        self.logTimestamp(self, method, episode, "pre call " + myCMD)
                         rc = os.system(myCMD)
-                        logTimestamp(self, method, episode, "post call " + myCMD)
+                        self.logTimestamp(self, method, episode, "post call " + myCMD)
                         myMSG = "CALLING CRM: <{0}> rc={1}".format(myCMD, rc)
                         self.tracer.info("{0}.{1}() {2}\n".format(self.__class__.__name__, method, myMSG))
-                        logTimestamp(self, method, episode, "send result to log")
+                        self.logTimestamp(self, method, episode, "send result to log")
 
                 # if self.cib_access != "all-off" and self.cib_access != "site-off":
                 # if self.cib_access == "all-on" or self.cib_access == "site-on" or self.cib_access == "glob-off":
                 if self.cib_access == "all-on" or self.cib_access == "site-on":
                     myCMD = "sudo /usr/sbin/crm_attribute -n hana_{0}_site_srHook_{1} -v {2} -t crm_config -s SAPHanaSR".format(mysid, mySite, mySRS)
-                    logTimestamp(self, method, episode, "pre call " + myCMD)
+                    self.logTimestamp(self, method, episode, "pre call " + myCMD)
                     rc = os.system(myCMD)
-                    logTimestamp(self, method, episode, "post call " + myCMD)
+                    self.logTimestamp(self, method, episode, "post call " + myCMD)
                     myMSG = "CALLING CRM: <{0}> rc={1}".format(myCMD, rc)
                     self.tracer.info("{0}.{1}() {2}\n".format(self.__class__.__name__, method, myMSG))
-                    logTimestamp(self, method, episode, "send result to log")
+                    self.logTimestamp(self, method, episode, "send result to log")
                     #
                     fallback_file_name = "../.crm_attribute.{0}".format(mySite)
                     fallback_stage_file_name = "../.crm_attribute.stage.{0}".format(mySite)
@@ -243,11 +244,11 @@ try:
                         # cluster attribute set was successfull - delete pending fallback file, if existing
                         try:
                             os.remove(fallback_file_name)
-                            logTimestamp(self, method, episode, "new event - pending fallback file {0} deleted".format(fallback_file_name))
+                            self.logTimestamp(self, method, episode, "new event - pending fallback file {0} deleted".format(fallback_file_name))
                         except FileNotFoundError:
                             pass
                     else:
-                        logTimestamp(self, method, episode, "update cluster attribute failed, enter fallback")
+                        self.logTimestamp(self, method, episode, "update cluster attribute failed, enter fallback")
                         #
                         # FALLBACK
                         # sending attribute to the cluster failed - using fallback method and write status to a file - RA to pick-up the value during next SAPHanaController monitor operation
@@ -258,18 +259,18 @@ try:
                         # cwd of hana is /hana/shared/<SID>/HDB00/<hananode> we use a relative path to cwd this gives us a <sid>adm permitted directory
                         #     however we go one level up (..) to have the file accessible for all SAP HANA swarm nodes
                         #
-                        logTimestamp(self, method, episode, "prepare fallback attribute file (stage)")
+                        self.logTimestamp(self, method, episode, "prepare fallback attribute file (stage)")
                         with open(fallback_stage_file_name, "w", encoding='utf-8') as fallbackFileObject:
                             fallbackFileObject.write("hana_{0}_site_srHook_{1} = {2}".format(mysid, mySite, mySRS))
-                        logTimestamp(self, method, episode, "created fallback attribute file (stage)")
+                        self.logTimestamp(self, method, episode, "created fallback attribute file (stage)")
                         #
                         # release the stage file to the original name (move is used to be atomic)
                         #      .crm_attribute.stage.<site> is renamed to .crm_attribute.<site>
                         #
-                        logTimestamp(self, method, episode, "move fallback attribute file stage to live")
+                        self.logTimestamp(self, method, episode, "move fallback attribute file stage to live")
                         os.rename(fallback_stage_file_name, fallback_file_name)
-                        logTimestamp(self, method, episode, "moved fallback attribute file stage to live")
-            logTimestamp(self, method, episode, "srConnectionChanged exit")
+                        self.logTimestamp(self, method, episode, "moved fallback attribute file stage to live")
+            self.logTimestamp(self, method, episode, "srConnectionChanged exit")
             return 0
 except NameError as e:
     print("Could not find base class ({0})".format(e))
